0584ea243de46e0b5288cc4b777ed42003b52805
snegard <stiande@stud.ntnu.no>
2018-03-19 14:22:04 +0100

    Added overloaded read functions in Functions.h

--- Functions.h
+++ Functions.h
@@ -3,6 +3,9 @@
 
 //Function declarations
 void writeMenu();
+char read(); //Reads a single char letter.
+void read(const char t[], char s[], const int LEN); //Reads non-empty text
+int read(const char t[], const int min, const int max); //Read int in interval
 
 
 #endif
