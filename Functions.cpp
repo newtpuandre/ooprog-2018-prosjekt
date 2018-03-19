711b93950294a967491f7133f4cdcfcdfb21966b
snegard <stiande@stud.ntnu.no>
2018-03-19 14:43:14 +0100

    Defined writeMenu and read functions in Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -2,6 +2,38 @@
 
 //Function definitions
 void writeMenu() {
-	//This functions will print out the menu for the user
+    cout << "\n\nAVAILABLE COMMANDS:"
+         << "\n\tS A | <no> | <name> - Display all players | Display player no | Display name"
+         << "\n\tI A | <name> - Display all sports | Display sport with name"
+         << "\n\tN S | I | D - New player | New sport | New division"
+         << "\n\tF S | I | D - Remove player | Remove sport | Remove division"
+         << "\n\tL - Display schedule or write schedule to file"
+         << "\n\tK - Display, or write to file, all the matches on a given date for a sport/ division"
+         << "\n\tT - Display, or write to file, sport standings or league standings"
+         << "\n\tR - Read reasults from file"
+         << "\n\tD - Display data about a player, sport or division"
+         << "\n\tE - Edit players within a team"
+         << "\n\tQ - Quit / avslutt";
+}
 
+char read() { //Reads a single char letter.
+    char ch;
+    cout << "\n\nCommand:  ";
+    cin >> ch;  cin.ignore();
+    return (toupper(ch));
+}
+
+void read(const char t[], char s[], const int LEN) { //Print text and reads non-empty text.
+    do {
+        cout << t << ": "; cin.getline(s, LEN);
+    } while (strlen(s) == 0);
+}
+
+int read(const char t[], const int min, const int max) { //Reads an int in given interval.
+    int n;
+    do {
+        cout << '\t' << t << " (" << min << '-' << max << "): ";
+        cin >> n; cin.ignore();
+    } while (n < min || n > max);
+    return n;
 }
