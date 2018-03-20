bbb75c9b2d2c7642350664c740efc7217772bf2c
snegard <stiande@stud.ntnu.no>
2018-03-20 09:49:26 +0100

    Defined consts and a enum in ConstEnum.h

--- ConstEnum.h
+++ ConstEnum.h
@@ -2,17 +2,25 @@
 #define  __CONSTENUM_H
 
 //Constants
-/*
-Uncomment this bit when be decide which is needed and its required size!
+/// Filenames are defined as consts for easier administration.
 
 const int NAMELEN = 80;
 const int STRLEN = 80;
+const int DATELEN = 9;
 const int MAXTEAM = 20;
-const int MAXPLAYERS = 20;
-const int DATELEN = 8;
-*/
+const int MAXPLAYERS = 50;
+const int MAXGOALS = 50;
+
+const char PLAYERS.DTA[] = "PLAYERS.DTA";
+const char SPORTS.DTA[] = "SPORTS.DTA";
+const char NEWDIV.DTA[] = "NEWDIVISION.DTA";
+const char RESULTS.DTA[] = "RESULTS.DTA";
+const char SCHEDULE.DTA[] = "SCHEDULE.DTA";
+const char MATCHDATE.DTA[] = "MATCHDATE.DTA";
+const char STANDINGS.DTA[] = "STANDINGS.DTA";
+
 
 //Enum
-
+enum tableType { 2-1-0, 3-1-0, 3-2-1-0 }
 
 #endif
